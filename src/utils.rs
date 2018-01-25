e1a5610628ceba56ad880d36e80b340dd7da9312
TheDjentleman <rhug90@gmail.com>
2018-01-25 16:28:42 +0100

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -10,6 +10,7 @@ pub trait ArrExt<T> {
     fn min_val(&self) -> &T; // calculates the minimum value of the slice
     fn max_val(&self) -> &T; // calculates the maximum value of the slcie
     fn abs(&self) -> Vec<T>;
+    fn clamp_zero(&self) -> Vec<T>;
 
     // inplace functions
     fn abs_inplace(&mut self);
@@ -20,7 +21,7 @@ pub trait ArrExt<T> {
 - we only want to implement these functions for vectors of ordered types (i.e. numbers) -> PartialOrd ensures that we have <, =<, > and >=
 - plus we want wo ensure that the abs function exists, we can use the Signed trait from the num crate for that
 */
-impl<T: PartialOrd + Signed> ArrExt<T> for [T] {  
+impl<T: PartialOrd + Signed + Copy> ArrExt<T> for [T] {  
     fn min_val(&self) -> &T {
         let min_val = self.iter().fold(None, |min, x| match min {
             None => Some(x),
@@ -42,6 +43,15 @@ impl<T: PartialOrd + Signed> ArrExt<T> for [T] {
     fn abs(&self) -> Vec<T> {
         let abs_wave: Vec<T> = self.iter().map(|val| val.abs()).collect();  
         abs_wave
+    }
+
+    // BLOG
+    // why the hell is there no trait bound for primitive types??
+    // shit only works when we have Copy trait (what is copy vs clone anyways 0o)
+    // need to use *val to dereference the shizzle
+    fn clamp_zero(&self) -> Vec<T> {
+        let clamped_wave: Vec<T> = self.into_iter().map(|val| if *val > T::zero() { *val } else { T::zero() }).collect();
+        clamped_wave
     }
 
     fn abs_inplace(&mut self) {
