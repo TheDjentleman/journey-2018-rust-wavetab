24854872c58aa82fdf509e9a382f5891b77e6a77
TheDjentleman <rhug90@gmail.com>
2018-01-26 12:21:11 +0100

    Update analysis.rs
    don't know what I do with what I did

--- src/analysis.rs
+++ src/analysis.rs
@@ -7,11 +7,11 @@ pub fn analyse_dsp(wave: &Vec<i16>, sample_rate: u32) {
 }
 
 pub fn analyse_genetic(wave: &Vec<i16>, sample_rate: u32) {
-
+    println!("{:?}, {}", wave, sample_rate);
 }
 
 pub fn analyse_mixed(wave: &Vec<i16>, sample_rate: u32) {
-    
+    println!("{:?}, {}", wave, sample_rate);
 }
 
 // we will try to detect "hard" jumps in the signal
@@ -26,6 +26,27 @@ fn count_notes(wave: &Vec<i16>, sample_rate: u32) -> u32 {
     //plotting::plot_wave(&abs_wave);
     let clamped_wave: Vec<i16> = wave.clamp_zero();
     plotting::plot_wave(&clamped_wave);
+
+    // calculate local maxima for each oscillation (is always located between zeros)
+    let mut peak_end = 0;
+    let mut peak_vals: Vec<i16> = Vec::new();
+    while peak_end < wave.len() {
+        let mut peak_start = peak_end;
+
+        // find start
+        while peak_start < wave.len() && wave[peak_start] <= 0 { peak_start += 1; }
+        if peak_start == wave.len() { break; }
+        peak_start -= 1; // set peak start back to last position that was 0, to prevent the first peak_end to not being out of range (which might happen when the signal is cut off at the end of the clip)
+
+        // peak end
+        peak_end = peak_start + 1;
+        while peak_end < wave.len() && wave[peak_end] > 0 { peak_end += 1; }
+        if peak_end == wave.len() { break; }
+
+        peak_vals.push(wave[peak_start..peak_end].max_val().clone());
+    }
+    println!("{:?}", peak_vals);
+
     /*
     let mut pos: usize = 0;
     while pos + window_size < abs_wave.len() {
